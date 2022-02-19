86816d58234fb29e73732af211e46dfed6905410
manny-uncharted <omoebun52@gmail.com>
2022-02-19 12:47:26 +0100

    worked with abstract classes

--- abstract-classes.py
+++ abstract-classes.py
@@ -4,6 +4,11 @@ It is a method that has a declaration but does not have an implementation.
 
 """
 
+'''
+How Abstract Base classes work : 
+By default, Python does not provide abstract classes. Python comes with a module that provides the base for defining Abstract Base classes(ABC) and that module name is ABC. ABC works by decorating methods of the base class as abstract and then registering concrete classes as implementations of the abstract base. A method becomes abstract when decorated with the keyword @abstractmethod.
+'''
+
 # Implementation
 
 from abc import ABC, abstractmethod
@@ -51,3 +56,45 @@ P.noofsides()
 
 H = Hexagon()
 H.noofsides()
+
+
+# Example 2
+# Python progrom showing abstract base class work
+
+from abc import ABC, abstractmethod
+class Animal(ABC):
+
+    def move(self):
+        pass
+
+class Human(Animal):
+    def move(self):
+        print("I can walk and run")
+
+class Dog(Animal):
+
+    def move(self):
+        print("I can bark")
+
+class Snake(Animal):
+
+    def move(self):
+        print("I can crawl")
+
+class Lion(Animal):
+     
+    def move(self):
+        print("I can roar")
+
+# Driver code
+Hu = Human()
+Hu.move()
+ 
+S = Snake()
+S.move()
+ 
+D = Dog()
+D.move()
+ 
+L = Lion()
+L.move()
